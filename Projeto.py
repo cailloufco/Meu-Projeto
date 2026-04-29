e8654d9359bd8ca0fd91f5583f82952e7656ee2b
cailloufco <caioextreme000@gmail.com>
2026-04-29 00:19:34 -0300

    mudanças no menu de cadastro

--- Projeto.py
+++ Projeto.py
@@ -3,38 +3,58 @@ adms = []
 clientes = []
 logado = False
 while True:
-    print('     *****MENU*****\n 1 - Login\n 2 - Cadastrar')
+    print('     *****MENU*****\n 1 - Login\n 2 - Cadastrar\n 0 - Encerrar')
     opcao = int(input('Digite a opção desejada: '))
+
     if opcao == 2:
         verif = int(input('Deseja criar um login de ADM ou Cliente?(1 - ADM, 2 - Cliente)'))
         if verif == 1:
             login = input('Crie seu nome de usuario: ')
             senha = input('Crie sua senha: ')
             adms.append([login,senha,True])
+
         elif verif == 2:
             login = input('Crie seu nome de usuario: ')
             senha = input('Crie sua senha: ')
             clientes.append([login,senha,False])
         continue
+
     elif opcao == 1:
         verif = int(input('Deseja entrar como ADM ou Cliente?(1 - ADM, 2 - Cliente)'))
+
         if verif == 1:
             login = input('Digite seu nome de usuario: ')
             senha = input('Digite sua senha de acesso: ')
             for adm in adms:
                 if login == adm[0] and senha == adm[1]:
                     logado = True
-                    
-                else:
-                    print('Login ou senha incorretos...')
-                    continue
-            print(logado , 'logado ou não')    
+                    break
+
+            if logado == False:
+                print('Login ou senha incorretos...')    
+            
+
         elif verif == 2:
             login = input('Digite seu nome de usuario: ')
             senha = input('Digite sua senha de acesso: ')
+            for cliente in clientes:
+                if login == cliente[0] and senha == cliente[1]:
+                    logado = True
+                    break
+            if logado == False:
+                print('Login ou senha incorretos...')    
+
+        elif verif == 0:
+            break
         
-while True:
-    print('     *****MENU*****\n 1 - Adicionar na lista\n 2 - Adicionar numa posição especifica\n 3 - Exibir Lista\n 4 - Buscar na lista \n 5 - Remover da Lista\n 0 - Encerrar\n')
+    if logado == True:
+        print('Login efetuado com sucesso')
+    
+    # isso daqui é temporario
+    elif logado == False:
+        print('algo está errado')
+    # ----------------------------
+    print('     *****MENU*****\n 1 - Adicionar na lista\n 2 - Adicionar numa posição especifica\n 3 - Exibir Lista\n 4 - Buscar na lista \n 5 - Remover da Lista\n 0 - Menu de Cadastro\n')
     opcao = int(input('Digite a opção desejada: '))
     if opcao == 1:
         nome = input('Digite o nome do aluno: ').lower()
@@ -64,5 +84,6 @@ while True:
         lista1[index] = novo
         print('Elemento atualizado com Sucesso!')               
     elif opcao == 0:
-        print('Encerrado.')
-        break
+        print('Retomando...')
+        logado = False
+        continue
