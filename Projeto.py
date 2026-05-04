3e1595baef71514135ae17d70fbedd1cc775d953
cailloufco <caioextreme000@gmail.com>
2026-05-04 00:47:18 -0300

    criação menu adm

--- Projeto.py
+++ Projeto.py
@@ -1,6 +1,7 @@
 lista1 =[]
 adms = []
 clientes = []
+user_logado = []
 logado = False
 while True:
     print('     *****MENU*****\n 1 - Login\n 2 - Cadastrar\n 0 - Encerrar')
@@ -28,6 +29,7 @@ while True:
             for adm in adms:
                 if login == adm[0] and senha == adm[1]:
                     logado = True
+                    user_logado = [[adm[0],adm[2]]]
                     break
 
             if logado == False:
@@ -40,6 +42,7 @@ while True:
             for cliente in clientes:
                 if login == cliente[0] and senha == cliente[1]:
                     logado = True
+                    user_logado = [[cliente[0] ,cliente[2]]]
                     break
             if logado == False:
                 print('Login ou senha incorretos...')    
@@ -54,36 +57,52 @@ while True:
     elif logado == False:
         print('algo está errado')
     # ----------------------------
-    print('     *****MENU*****\n 1 - Adicionar na lista\n 2 - Adicionar numa posição especifica\n 3 - Exibir Lista\n 4 - Buscar na lista \n 5 - Remover da Lista\n 0 - Menu de Cadastro\n')
-    opcao = int(input('Digite a opção desejada: '))
-    if opcao == 1:
-        nome = input('Digite o nome do aluno: ').lower()
-        lista1.append(nome)
-        print('Adicionado com Sucesso!')
-    elif opcao == 2:
-        nome = str(input('Digite o nome do aluno: ')).lower()
-        pos = int(input('Em qual posição você deseja inserir?\n'))
-        lista1.insert(pos,nome)
-        print('Adicionado com Sucesso!')
-    elif opcao == 3:
-        print(lista1)
-    elif opcao == 4:
-        busca = input('Quem você deseja buscar?\n').lower()
-        for i in range(len(lista1)):
-            if busca == lista1[i]:
-                index = i
-                print(f'A pessoa está na lista na posição {index}.')
-                break
-    elif opcao == 5:
-        remov = int(input('Digite qual a posição da lista deseja remover: '))
-        lista1.pop(remov)
-        print('Removido com Sucesso!')
-    elif opcao == 6:
-        index = int(input('Qual item da lista deseja atualizar?'))
-        novo = input('Pelo o que você deseja substituir?\n')
-        lista1[index] = novo
-        print('Elemento atualizado com Sucesso!')               
-    elif opcao == 0:
-        print('Retomando...')
-        logado = False
-        continue
+
+    '''-----------Requisitos Funcionais (ADM):
+
+R1 - Login: Efetuar login com usuário e senha para acessar o menu de gestão da fazenda. Tanto ADM ou CLIENTE pode fazer login.
+
+R2 - Gerenciar Rebanho: Cadastrar, buscar, atualizar e remover animais. O cadastro deve incluir o tipo do animal (Bovino de Leite, Caprino, Ovino, Suíno/Leitão), identificação (brinco/número) e status (ex: em lactação, para engorda, disponível para venda).
+
+R3 - Gerenciar Produção e Derivados: Cadastrar a produção diária. O ADM deve poder registrar litros de leite ordenhados e adicionar ao estoque produtos fabricados (ex. Queijo Coalho, Queijo Manteiga), informando o peso (kg) e o valor de venda.
+
+R4 - Tema Livre (ADM): Criar uma funcionalidade útil para o produtor rural.
+    '''
+    animais_cadastrados = []
+    if user_logado and user_logado[0][1] == True:
+        while True:
+            print(f'\n\n\n\nBem-vindo, {user_logado[0][0]} \n     *****MENU*****\n 1 - Gerenciar Rebanho\n 2 - Gerenciar Produção e Derivados\n 0 - Menu de Cadastro\n')
+            opcao = int(input('Digite a opção desejada: '))
+            if opcao == 1:
+                while True:
+                    print('''     *****MENU*****
+        1 - Cadastrar
+        2 - Buscar
+        3 - Atualizar
+        4 - Remover Animais
+        0 - Voltar''')
+                    controle = int(input('Digite a opção desejada: '))
+                    if controle == 0:
+                        break
+            if opcao == 2:
+                while True:
+                    print('''     *****MENU*****
+        1 - Cadastrar Produção Diária
+        2 - Registrar Litros de Leite Ordenhados
+        3 - adicionar ao Estoque Produtos Fabricados
+        0 - Voltar''')
+                    controle = int(input('Digite a opção desejada: '))
+                    if controle == 0:
+                        break
+                
+                    
+            elif opcao == 0:
+                print('Retomando...')
+                logado = False
+                user_logado = []
+                continue
+    
+    elif user_logado and user_logado[0][1] != True and logado == True:
+    # menu do cliente
+    
+        print()
