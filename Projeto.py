00f30b73315c6289cc42d8811f0298c4a9388e99
cailloufco <caioextreme000@gmail.com>
2026-05-16 12:32:20 -0300

    clone

--- Projeto.py
+++ Projeto.py
@@ -1,26 +1,31 @@
 lista1 =[]
-adms = []
-clientes = []
+adms = [['higor','123',True]]
+clientes = [['caio','123',False]]
 user_logado = []
+animais_cadastrados = []
+produtos_cadastrados = [['OVO',10,'VENDA']]
+ListaTipos = []
+ListaStatus = []
+index = 0
 logado = False
 while True:
-    print('     *****MENU*****\n 1 - Login\n 2 - Cadastrar\n 0 - Encerrar')
+    print('     *****MENU*****\n 1 - Cadastrar\n 2 - Login\n 0 - Encerrar')
     opcao = int(input('Digite a opção desejada: '))
 
-    if opcao == 2:
-        verif = int(input('Deseja criar um login de ADM ou Cliente?(1 - ADM, 2 - Cliente)'))
+    if opcao == 1:
+        verif = int(input('Deseja criar um conta de ADM ou Cliente?(1 - ADM, 2 - Cliente)'))
         if verif == 1:
             login = input('Crie seu nome de usuario: ')
             senha = input('Crie sua senha: ')
             adms.append([login,senha,True])
-
+        
         elif verif == 2:
             login = input('Crie seu nome de usuario: ')
             senha = input('Crie sua senha: ')
             clientes.append([login,senha,False])
         continue
 
-    elif opcao == 1:
+    elif opcao == 2:
         verif = int(input('Deseja entrar como ADM ou Cliente?(1 - ADM, 2 - Cliente)'))
 
         if verif == 1:
@@ -34,7 +39,7 @@ while True:
 
             if logado == False:
                 print('Login ou senha incorretos...')    
-            
+                continue
 
         elif verif == 2:
             login = input('Digite seu nome de usuario: ')
@@ -42,22 +47,17 @@ while True:
             for cliente in clientes:
                 if login == cliente[0] and senha == cliente[1]:
                     logado = True
-                    user_logado = [[cliente[0] ,cliente[2]]]
+                    user_logado = [cliente[0] ,cliente[2]]
                     break
             if logado == False:
                 print('Login ou senha incorretos...')    
-
-        elif verif == 0:
-            break
+                continue
+    elif verif == 0:
+        break
         
     if logado == True:
         print('Login efetuado com sucesso')
     
-    # isso daqui é temporario
-    elif logado == False:
-        print('algo está errado')
-    # ----------------------------
-
     '''-----------Requisitos Funcionais (ADM):
 
 R1 - Login: Efetuar login com usuário e senha para acessar o menu de gestão da fazenda. Tanto ADM ou CLIENTE pode fazer login.
@@ -68,34 +68,178 @@ R3 - Gerenciar Produção e Derivados: Cadastrar a produção diária. O ADM dev
 
 R4 - Tema Livre (ADM): Criar uma funcionalidade útil para o produtor rural.
     '''
-    animais_cadastrados = []
     if user_logado and user_logado[0][1] == True:
         while True:
-            print(f'\n\n\n\nBem-vindo, {user_logado[0][0]} \n     *****MENU*****\n 1 - Gerenciar Rebanho\n 2 - Gerenciar Produção e Derivados\n 0 - Menu de Cadastro\n')
+            print(f'{"\n"*4}Bem-vindo, {user_logado[0][0]} \n     *****MENU*****\n 1 - Gerenciar Rebanho\n 2 - Gerenciar Produção e Derivados\n 0 - Menu de Cadastro\n')
             opcao = int(input('Digite a opção desejada: '))
+            
             if opcao == 1:
                 while True:
                     print('''     *****MENU*****
-        1 - Cadastrar
-        2 - Buscar
-        3 - Atualizar
-        4 - Remover Animais
-        0 - Voltar''')
+1 - Cadastrar
+2 - Buscar
+3 - Atualizar
+4 - Remover Animais
+0 - Voltar''')
                     controle = int(input('Digite a opção desejada: '))
                     if controle == 0:
+                        print('Voltando ao meu')
                         break
-            if opcao == 2:
+                    
+                    elif controle == 1:
+                        Tipo = str(input("Qual o tipo de animal?\n")).upper()                        
+                        indetificacao = int(input('Qual o número de indentificação?\n'))
+                        status = input('Qual o status do animal?\n').upper()                     
+                        animais_cadastrados.append([Tipo,indetificacao,status])
+                    
+                    elif controle == 2:
+                        while True:
+                            print(f'{"\n"*4}O que você deseja buscar, {user_logado[0][0]}? \n     *****MENU*****\n 1 - Busca por ID do animal\n 2 - Buscar por Tipo dos animais\n 3 - Buscar por status\n 0 - Sair da busca\n')
+                            opcao = int(input('Digite a opção desejada: '))
+                            
+                            if opcao == 1:
+                                busca = input('Digite o número do animal: ')
+                                for i in range(len(animais_cadastrados)):
+                                    if busca == animais_cadastrados[i][1]:
+                                        index = i
+                                        break
+                                print(f'O seu animal {animais_cadastrados[index]}')
+                            
+                            elif opcao == 2:
+                                busca = str(input('Digite o Tipo do animal: ')).upper()
+                                for i in range(len(animais_cadastrados)):                              
+                                    if busca == animais_cadastrados[i][0]:
+                                        ListaTipos.append = [animais_cadastrados[i]]
+                                        
+                                print(f'Os seus {busca}s são:\n{ListaTipos}')
+                                print(f'Você tem {len(ListaTipos)} {busca}s')
+                                ListaTipos.clear()
+                            
+                            elif opcao == 3:                                
+                                busca = str(input('Digite o Status do animal que deseja buscar: ')).upper()
+                                for i in range(len(animais_cadastrados)):                              
+                                    if busca == animais_cadastrados[i][2]:
+                                        ListaStatus.append = [animais_cadastrados[i]]
+                                print(f'Você tem {len(ListaStatus)} animais em: {busca}')
+                                print(f'Os seus animais em {busca}s são:\n{ListaStatus}')
+                                ListaStatus.clear()
+                            elif opcao == 0:
+                                print('Encerrando busca.')
+                                break
+                            else:
+                                print('Opçao inválida')
+                                continue
+                    
+                    elif controle == 3:
+                        busca = int(input('Qual o número de identificação do animal que deseja atualizar?\n'))
+                        for i in range(len(animais_cadastrados)):
+                                    if busca == animais_cadastrados[i][1]:
+                                        index = i
+                                        break
+                        a = input(f'O status atual do animal é: {animais_cadastrados[index][2]}. Deseja Atulizar?(S - Sim N - Não)').upper()
+                        if a == 'S':
+                            b = input('Digite o status atual do animal: ').upper()
+                            animais_cadastrados[index][2] = b
+                        elif a == 'N':
+                            print('Retornando ao menu')
+                            break
+                    
+                    elif controle == 4:
+                        busca = int(input('Qual o número de identificação do animal que deseja remover?\n'))
+                        for i in range(len(animais_cadastrados)):
+                                    if busca == animais_cadastrados[i][1]:
+                                        index = i
+                                        break
+                        a = input(f'Os dados do animal é: {animais_cadastrados[index]}. Deseja Remover?(S - Sim N - Não)').upper()
+                        if a == 'S':
+                            animais_cadastrados.pop[index]
+                        elif a == 'N':
+                            print('Retornando ao menu')
+                            break
+                    elif controle == 0:
+                        break
+                    else:
+                        print('Opção inválida!!!')
+                        continue
+            elif opcao == 2:
                 while True:
                     print('''     *****MENU*****
-        1 - Cadastrar Produção Diária
-        2 - Registrar Litros de Leite Ordenhados
-        3 - adicionar ao Estoque Produtos Fabricados
-        0 - Voltar''')
+1 - Cadastrar
+2 - Buscar
+3 - Atualizar Itens
+4 - Remover Itens
+0 - Voltar''')
                     controle = int(input('Digite a opção desejada: '))
                     if controle == 0:
+                        print('Voltando ao meu')
                         break
-                
                     
+                    elif controle == 1:
+                        Tipo = str(input("Qual o tipo de Produto?\n")).upper()                        
+                        Quantidade = int(input('Qual a quantidade do Produto no estoque?\n'))
+                        status = input('Qual o status do produto?\n').upper()                     
+                        produtos_cadastrados.append([Tipo,Quantidade,status])
+                    
+                    elif controle == 2:
+                        while True:
+                            print(f'{"\n"*4}O que você deseja buscar, {user_logado[0][0]}? \n     *****MENU*****\n 1 - Busca por Nome do Produto\n 2 - Buscar por status\n 0 - Sair da busca\n')
+                            opcao = int(input('Digite a opção desejada: '))
+                            
+                            if opcao == 1:
+                                busca = input('Digite o nome do Produto: ')
+                                for i in range(len(produtos_cadastrados)):
+                                    if busca == produtos_cadastrados[i][0]:
+                                        index = i
+                                        break
+                                print(f'O situação do seu produto: {produtos_cadastrados[index]}')
+
+                            elif opcao == 2:                                
+                                busca = str(input('Digite o Status dos produtos que deseja buscar: ')).upper()
+                                for i in range(len(produtos_cadastrados)):                              
+                                    if busca == produtos_cadastrados[i][2]:
+                                        ListaStatus.append = [produtos_cadastrados[i]]
+                                print(f'Você tem {len(ListaStatus)} produtos em: {busca}')
+                                print(f'Os seus produtos em {busca}s são:\n{ListaStatus}')
+                                ListaStatus.clear()
+                            elif opcao == 0:
+                                print('Encerrando busca.')
+                                break
+                            else:
+                                print('Opçao inválida')
+                                continue
+                    
+                    elif controle == 3:
+                        busca = int(input('Qual o nome do produto que deseja atualizar?\n'))
+                        for i in range(len(produtos_cadastrados)):
+                                    if busca == produtos_cadastrados[i][0]:
+                                        index = i
+                                        break
+                        a = input(f'O status atual do produto é: {produtos_cadastrados[index][2]}. Deseja Atulizar?(S - Sim N - Não)').upper()
+                        if a == 'S':
+                            b = input('Digite o novo status do produto: ').upper()
+                            produtos_cadastrados[index][2] = b
+                        elif a == 'N':
+                            print('Retornando ao menu')
+                            break
+                    
+                    elif controle == 4:
+                        busca = int(input('Qual o nome do produto que deseja remover?\n'))
+                        for i in range(len(produtos_cadastrados)):
+                                    if busca == produtos_cadastrados[0][1]:
+                                        index = i
+                                        break
+                        a = input(f'Os dados do produto são: {animais_cadastrados[index]}. Deseja Remover?(S - Sim N - Não)').upper()
+                        if a == 'S':
+                            animais_cadastrados.pop[index]
+                        elif a == 'N':
+                            print('Retornando ao menu')
+                            break
+                    elif controle == 0:
+                        break
+                    else:
+                        print('Opção inválida!!!')
+                        continue
+   
             elif opcao == 0:
                 print('Retomando...')
                 logado = False
